2b8b682c1d6fd84b8daa6c9e12d27bc023025dd5
Ramses Contreras <ramses.contreras7@gmail.com>
2022-02-04 23:49:21 -0800

    CityNode class implemented + Code compiling and running well
    Code seems to be working properly. Still need to implement unittesting

--- src/CityNode.java
+++ src/CityNode.java
@@ -1,1 +1,22 @@
+/**
+ * CityNode: Represents a node in the map stored in the FlightMap instance. Contains the name of the city and
+ * the name of the parent city in the route.
+ */
+public class CityNode {
+    /**
+     * Identifier of the city
+     */
+    String name;
+    /**
+     * City from which the flight arrived
+     */
+    String parent;
 
+    /**
+     * Creates a new city node given name of city and of parent city
+     */
+    public CityNode(String name, String parent){
+        this.name = name;
+        this.parent = parent;
+    }
+}
